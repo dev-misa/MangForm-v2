a08076b818c9d3a06ddaec0d7372d03575624802
Misael Taperia <askedmisael2@gmail.com>
2025-03-20 14:31:11 -0500

    testing

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+      "./index.html",      // Include the path to your HTML files
+      "./src/**/*.{js,jsx,ts,tsx}",  // Include the path to your JavaScript/TypeScript files (if applicable)
+      // Add more paths as needed (e.g., for React or Vue files)
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
